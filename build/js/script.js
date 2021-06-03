76dee455e3c8b1d28572846080776f2eaa2e1f99
unknown <a.sekret@wssupport.ru>
2021-06-03 09:58:20 +0300

    правки 67920

--- build/js/script.js
+++ build/js/script.js
@@ -1911,46 +1911,6 @@ $('body').on('click', '[data-modal]', function () {
     modalClass.render();
 })
 
-function pagination() {
-    $('body').on('click', '.js-pagination', function (evt) {
-        evt.preventDefault();
-        preloader.show();
-
-        var data = {page: $(this).attr('data-num-page')}
-
-        function onSuccess(data) {
-            if(data.status == 'OK') {
-                $('.js-page-content').replaceWith(data.template);
-
-            } else {
-                alert('Ошибка связи с сервером');
-            }
-            preloader.hide();
-        }
-
-        onSendAjax({
-            type: 'POST',
-            url: 'action.php',
-            dataType: 'json',
-            data: data,
-            onSuccess: function (data) {
-                //удалить при интеграции
-                var data = {
-                    status: 'OK',
-                    template: $('#filter').hasClass('persons-list--registry') ? mockDataPersonsList.clone() : mockUserList.clone(),
-                }
-                onSuccess(data);
-            },
-
-            onError: function () {
-                alert('Ошибка связи с сервером');
-                preloader.hide();
-            },
-        })
-    })
-}
-
-pagination();
 // поставить галочку в реестре пациентов
 $('body').on('click', '.js-checked', function (evt) {
     var input = $(this).find('input');
@@ -2009,6 +1969,46 @@ $('body').on('click', '.js-locked', function (evt) {
         })
     }
 })
+function pagination() {
+    $('body').on('click', '.js-pagination', function (evt) {
+        evt.preventDefault();
+        preloader.show();
+
+        var data = {page: $(this).attr('data-num-page')}
+
+        function onSuccess(data) {
+            if(data.status == 'OK') {
+                $('.js-page-content').replaceWith(data.template);
+
+            } else {
+                alert('Ошибка связи с сервером');
+            }
+            preloader.hide();
+        }
+
+        onSendAjax({
+            type: 'POST',
+            url: 'action.php',
+            dataType: 'json',
+            data: data,
+            onSuccess: function (data) {
+                //удалить при интеграции
+                var data = {
+                    status: 'OK',
+                    template: $('#filter').hasClass('persons-list--registry') ? mockDataPersonsList.clone() : mockUserList.clone(),
+                }
+                onSuccess(data);
+            },
+
+            onError: function () {
+                alert('Ошибка связи с сервером');
+                preloader.hide();
+            },
+        })
+    })
+}
+
+pagination();
 function Preloader() {
     this.template = $('<div class="preloader">\n' +
         '        <div class="preloader__in"></div>\n' +
